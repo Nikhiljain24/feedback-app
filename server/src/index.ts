4e304c83828690600a0f6cd5470124a9494074bd
DESKTOP-JO0VIC9\Jaya <nikhilchandaliya24@gmail.com>
2026-02-12 21:51:49 +0530

    add backend in server directory

---
+++ server/src/index.ts
@@ -0,0 +1,24 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Feedback App Server is running!');
+});
+
+app.get('/api/health', (req: Request, res: Response) => {
+    res.json({ status: 'ok', message: 'Server is running' });
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
